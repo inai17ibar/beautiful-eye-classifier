4a670ca0e25d3fd9847d5d6714b36a10bef1c44e
Soichiro Inatani <inai17ibar@gmail.com>
2023-11-01 18:47:39 +0900

    保存されるファイル名の変更。

--- crawler/download_images_from_url.py
+++ crawler/download_images_from_url.py
@@ -2,6 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from urllib.parse import urljoin
+import cv2
+import numpy as np
+import datetime
+
+def contains_face(image_data):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+    nparr = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # 画像が正常にデコードされているか確認
+    if image is None:
+        print("Failed to decode the image.")
+        return False
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return len(faces) > 0
 
 def download_images_from_url(url, folder_name, limit=100):
     """
@@ -29,17 +47,27 @@ def download_images_from_url(url, folder_name, limit=100):
         # 相対URLを絶対URLに変換
         img_url = urljoin(url, img_url)
 
+         # 元のファイル名を取得
+        original_filename = img_url.split('/')[-1].split('?')[0]  # URLの最後の部分を取得して、クエリパラメータを除外
+
+        # 現在の日時を取得
+        now = datetime.datetime.now()
+        timestamp = now.strftime('%Y%m%d%H%M%S')
+
+        # 保存する画像名を作成
+        save_filename = f"{timestamp}_{original_filename}"
+
         # URLから画像を取得
         response = requests.get(img_url, stream=True)
         if response.status_code == 200:
-            # 画像を保存
-            with open(os.path.join(folder_name, f"image_{downloaded}.jpg"), 'wb') as f:
-                for chunk in response.iter_content(1024):
-                    f.write(chunk)
-            
-            downloaded += 1
-            if downloaded >= limit:
-                break
+            image_data = response.content
+            # 画像に顔が含まれているか確認
+            if contains_face(image_data):
+                with open(os.path.join(folder_name, save_filename), 'wb') as f:
+                    f.write(image_data)
+                downloaded += 1
+                if downloaded >= limit:
+                    break
 
     print(f"{downloaded} images downloaded!")
 
